f2b46a3a2134a6528c5a0adc97810c3186df1239
Ziaul Kamal <codepad@usa.com>
2024-08-07 12:40:11 +0700

    update

--- src/app/lib/firebase.js
+++ src/app/lib/firebase.js
@@ -1,15 +1,19 @@
 // src/lib/firebase.js
 import admin from 'firebase-admin';
 
-// Parse JSON credentials from environment variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS || '{}');
+try {
+  // Parse JSON credentials from environment variable
+  const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS || '{}');
 
-// Initialize Firebase Admin SDK if not already initialized
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_URL_DB
-  });
+  // Initialize Firebase Admin SDK if not already initialized
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: process.env.FIREBASE_URL_DB
+    });
+  }
+} catch (error) {
+  console.error('Error initializing Firebase Admin SDK:', error);
 }
 
 export { admin };
